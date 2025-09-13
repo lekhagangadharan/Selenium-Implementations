d4c467eec192386a58f07b7104763acf62660f5a
Lekha Gangadharan <78950627+lekhagangadharan@users.noreply.github.com>
2025-09-13 12:20:15 -0400

    Week1-Day1 Assignment

---
+++ week1/day1/Chrome.java
@@ -0,0 +1,27 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		
+		//version of type float with a value of 91.0f
+		float version = 91.0f;
+		
+		//developer of type Stringwith a value of "Google"
+		String developer="Google";
+		
+		//isBeta of type booleanwith a value of false
+		boolean isBeta=false;
+		
+		//releaseYear of type int with a value of 2008
+		int releaseYear = 2008;
+		
+		//shortcutKey of type charwith a value of 'C'
+		char shortcutKey ='C';
+		
+		System.out.println(version +" "+ developer +" "+isBeta+" " +releaseYear+" "+shortcutKey);
+		 
+
+	}
+
+}
---
+++ week1/day1/HelloTestLeaf.java
@@ -0,0 +1,12 @@
+package week1.day1;
+
+import cucumber.api.cli.Main;
+
+public class HelloTestLeaf {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to TestLeaf");
+	}
+
+}
